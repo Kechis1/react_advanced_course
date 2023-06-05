eb628db9cc13322b98260d58ab01ecb9843773c9
danielbill <danielbill95@gmail.com>
2023-06-05 16:53:19 +0200

    context

--- src/components/CurrentUserInfo.tsx
+++ src/components/CurrentUserInfo.tsx
@@ -5,8 +5,8 @@ export function CurrentUserInfo() {
 
 	return (
 		<div className={"mt-5"}>
-			<span className={"text-3xl"}>Username: <span className={"text-blue-500"}>{user?.username}</span></span>,
-			<span className={"text-3xl mx-2"}>Email: <span className={"text-blue-500"}>{user?.email}</span></span>
+			<div className={"text-2xl"}>Username: <span className={"text-blue-500"}>{user?.username}</span></div>
+			<div className={"text-2xl my-2"}>Email: <span className={"text-blue-500"}>{user?.email}</span></div>
 			<button className={"rounded p-2 bg-blue-500 text-white font-bold"} onClick={logout}>Logout</button>
 		</div>
 	);
--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -7,8 +7,8 @@ export function LoginForm() {
 	const [email, setEmail] = useState('');
 
 	return <div className={"mt-4"}>
-		<input className={"rounded border-2 p-2"} placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-		<input className={"rounded border-2 p-2 mx-2"} placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+		<input type={"text"} className={"rounded border-2 p-2"} placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+		<input type={"email"} className={"rounded border-2 p-2 mx-2"} placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
 		<button className={"rounded p-2 bg-blue-500 text-white font-bold"} onClick={() => login({username, email})}>Login</button>
 	</div>
