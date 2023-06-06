ff698597672c20ad1a93d3b4bb25fa69f51887bb
danielbill <danielbill95@gmail.com>
2023-06-06 18:57:58 +0200

    testing

--- src/tests/jokes/Joke.test.tsx
+++ src/tests/jokes/Joke.test.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import {act, fireEvent, render, screen, waitFor} from "@testing-library/react";
 import {ChuckNorrisJoke} from "../../components/ChuckNorrisJoke";
 
 jest.mock('axios');
@@ -7,27 +7,28 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 const axiosResponse  = {data: {value: 'test'}};
 
 beforeEach(() => {
-    mockedAxios.get.mockResolvedValueOnce(axiosResponse);
+    mockedAxios.get.mockResolvedValue(axiosResponse);
 });
 
 describe('ChuckNorrisJoker component', () => {
 
     it('should display the load next button', async function () {
-        render(<ChuckNorrisJoke/>);
+        await act(async () => {
+            render(<ChuckNorrisJoke/>);
+        });
+
         const button = screen.getByRole('button');
         expect(button).toHaveTextContent('Load another');
     })
 
-    it('should disable the load next button upon initialization', async function () {
-        render(<ChuckNorrisJoke/>);
-        const button = screen.getByRole('button');
-        expect(button).toBeDisabled();
-    })
-
     it('should disable the load next button while fetching data', async function () {
         render(<ChuckNorrisJoke/>);
-        fireEvent.click(screen.getByRole('button'));
-        expect(screen.getByRole('button')).toBeDisabled();
+        const button = screen.getByRole('button');
+        await act(async () => {
+            fireEvent.click(button);
+            expect(button).toBeDisabled()
+        });
+        await waitFor(() => expect(button).not.toBeDisabled());
     })
 
     it('should enable the load next button after fetching data', async function () {
@@ -39,10 +40,12 @@ describe('ChuckNorrisJoker component', () => {
         });
     })
 
-    it('should display no joke upon initialization', async function () {
-        render(<ChuckNorrisJoke/>);
-        const h2Element = screen.getByRole('heading', {level: 2});
-        expect(h2Element).toHaveTextContent('');
+    it('should display a joke', async function () {
+        await act(async () => {
+            render(<ChuckNorrisJoke/>);
+        });
+        const h2Element = await screen.findByRole('heading', {level: 2});
+        expect(h2Element).toBeInTheDocument();
     });
 
     it('should display the joke after fetching data', async function () {
